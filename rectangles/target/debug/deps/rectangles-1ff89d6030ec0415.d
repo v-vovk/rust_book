5e29527b77a366344b2f408e446ca55bf5f8aa20
Vlad Vovk <wolfykey@gmail.com>
2024-06-26 19:38:41 +0200

    rectangles

---
+++ rectangles/target/debug/deps/rectangles-1ff89d6030ec0415.d
@@ -0,0 +1,5 @@
+/Users/dev/projects/rust/rust_book/rectangles/target/debug/deps/rectangles-1ff89d6030ec0415: src/main.rs
+
+/Users/dev/projects/rust/rust_book/rectangles/target/debug/deps/rectangles-1ff89d6030ec0415.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/Users/dev/projects/rust/rust_book/rectangles/target/debug/rectangles: /Users/dev/projects/rust/rust_book/rectangles/src/main.rs
